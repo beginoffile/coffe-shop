6758c6c34941af6000daf4030dc484afa6df2d26
beginoffile <cursos.mapatupisaro@gmail.com>
2023-03-22 23:13:40 -0600

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const {src, dest, watch} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function css(done){
+    // compilar sass
+    // 1.- identificar archivo, 2.- Compilarla, 3.- Guardar el .css
+    src('src/scss/app.scss')
+        .pipe(sass())
+        .pipe(dest('build/css'))
+    done();
+}
+
+function dev(){
+    watch('src/scss/app.scss',css);
+}
+
+exports.css = css;
+exports.dev = dev;
