8069f6734bf2d88c78ec4b78ed0ad67fa7dbb940
beginoffile <cursos.mapatupisaro@gmail.com>
2023-03-23 22:35:38 -0600

    update project

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,23 @@
-const {src, dest, watch} = require('gulp');
+const {src, dest, watch, series} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 function css(done){
     // compilar sass
     // 1.- identificar archivo, 2.- Compilarla, 3.- Guardar el .css
     src('src/scss/app.scss')
         .pipe(sass())
+        .pipe( postcss([autoprefixer()]) )
         .pipe(dest('build/css'))
     done();
 }
 
 function dev(){
-    watch('src/scss/app.scss',css);
+    watch('src/scss/**/*.scss', css);
+    // watch('src/scss/app.scss',css);
 }
 
 exports.css = css;
 exports.dev = dev;
+exports.default = series(css,dev);
