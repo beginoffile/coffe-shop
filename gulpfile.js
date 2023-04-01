547ac19de9944dc118f9073945bd5ef730c1b61f
beginoffile <cursos.mapatupisaro@gmail.com>
2023-03-31 20:25:11 -0600

    images optimization

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@ const autoprefixer = require('autoprefixer');
 
 // Imagenes
 const imagemin = require('gulp-imagemin');
-
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 
 function css(done){
@@ -27,6 +28,24 @@ function imagenes( done ){
     done();
 }
 
+function versionWebp(){
+    const opciones = {
+        quality:50
+    }
+    return src('src/img/**/*.{png,jpg}')
+    .pipe(webp(opciones))
+    .pipe(dest('build/img/'));
+}
+
+function versionAvif(){
+    const opciones = {
+        quality:50
+    }
+    return src('src/img/**/*.{png,jpg}')
+    .pipe(avif( opciones ))
+    .pipe( dest('build/img'));
+}
+
 function dev(){
     watch('src/scss/**/*.scss', css);
     watch('src/img/**/*', imagenes);
@@ -36,4 +55,6 @@ function dev(){
 exports.css = css;
 exports.dev = dev;
 exports.imagenes = imagenes;
-exports.default = series(imagenes, css,dev);
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.default = series(imagenes, versionWebp, versionAvif, css,dev);
